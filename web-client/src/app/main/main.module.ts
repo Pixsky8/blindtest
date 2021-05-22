f5ba21d23aff1e49a501c3f25fecc54cf217ae7f
Pixsky8 <pixsky8@gmail.com>
2021-05-22 22:58:07 +0200

    client: scoreboard display

--- web-client/src/app/main/main.module.ts
+++ web-client/src/app/main/main.module.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+
 import { QuestionComponent } from "./question/question.component";
 import { ScoreboardComponent } from "./scoreboard/scoreboard.component";
 
 @NgModule({
+    imports: [CommonModule],
     declarations: [
         QuestionComponent,
         ScoreboardComponent
