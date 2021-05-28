2d17c92c850323938dbb8676f9093548d235d0d4
Pixsky8 <pixsky8@gmail.com>
2021-05-29 00:43:27 +0200

    proxy: refactor

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 ### DOCKER SETUP ###
 docker network create --subnet=172.18.0.0/16 blindtest
 
-docker build -t blindtest_nginx nginx
+docker build -t blindtest_proxy proxy
 docker build -t blindtest_server server
 
 ### ANGULAR SETUP ###
